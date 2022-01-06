3f0812100d4a2da474186305f333a83f08829288
Matteo De Carlo <matteo.dek@gmail.com>
2022-01-06 14:53:42 +0100

    Simplified example code
    renamed binary from Tests to WickedSpaceFlight

--- WickedSpaceFlight.h
+++ WickedSpaceFlight.h
@@ -4,18 +4,10 @@
 
 class TestsRenderer : public RenderPath3D
 {
-	wiLabel label;
-	wiComboBox testSelector;
 	wiECS::Entity ik_entity = wiECS::INVALID_ENTITY;
 public:
 	void Load() override;
 	void Update(float dt) override;
-	void ResizeLayout() override;
-
-	void RunJobSystemTest();
-	void RunFontTest();
-	void RunSpriteTest();
-	void RunNetworkTest();
 };
 
 class WickedSpaceFlight : public MainComponent
